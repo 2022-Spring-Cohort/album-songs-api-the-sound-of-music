ff302ca6e1f3e5f7ba4ecdf23d635d87dc208149
di5781 <diane@hault.com>
2022-03-17 15:24:08 -0400

    add all the entry fields

--- frontend-spa/src/js/header.js
+++ frontend-spa/src/js/header.js
@@ -1,6 +1,6 @@
 export default function header() {
   return `
-  <header class ='header-title'><h1>  Welcome to the Sound of Music!</h1></header>
+  <header class ='header-title'><h1  style="color: darkblue">  Welcome to the Sound of Music!</h1></header>
  
   `;
 }
--- frontend-spa/src/js/home.js
+++ frontend-spa/src/js/home.js
@@ -3,19 +3,29 @@ export default function home(albums) {
 <main class="main-content">
 <section class="album-list">
 ${albums
-  .map((album) => {
-    return `<div class="albums">
-    <h2 class="album-title" >${album.title} </h2>
-    <image src = "${album.image}" alt = "picture of the album"/>
-    <h3 class="album-record-label" >${album.recordLabel}</h3>
-    <h4 class="album-ratings" >${album.ratings}</h4>
+    .map((album) => {
+        return `<div class="albums">
+          <h2 class="album-title" >Title: ${album.title} </h2>
+          <p class="album-record-label" >Label: ${album.recordLabel}</p>
+          <p class="album-ratings" >Ratings: ${album.ratings}</p>
+          <image class="album-img" src = "${album.image}" alt = "picture of the album" width="200" height="200" />
+          <input type="hidden" class="id_album" value="${album.id}" >
+        </div>`;
+    }).join("")
+  }  
 
-  
- 
-</div>`;
-  })
-  .join("")}    
+  <section class="album-input">
+  <input type="text" placeholder="Album Title: " class="albumTitleInput" />
+  <input type="text" placeholder="Album Image: " class="albumImageInput" />
+  <input type="text" placeholder="Record Label: " class="albumRecordLabelInput" />
+  <input type="text" placeholder="Album Ratings: " class="albumRatingsInput" />
+  <button class="addAlbumButton">Add album"</button>
+
+</section>
 </section>
 </main>
 `;
 }
+
+
+
